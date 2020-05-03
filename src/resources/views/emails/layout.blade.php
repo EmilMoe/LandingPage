6c02cf257d507b19b9a4b6bcb54e3f3a23a2663e
Emil Moe <emil@moegroup.dk>
2020-05-03 20:17:43 +0200

    bugfix

--- src/resources/views/emails/layout.blade.php
+++ src/resources/views/emails/layout.blade.php
@@ -1,14 +1,7 @@
-@if(\View::exists('emails.layout'))
-    @extends('emails.layout')
-    @section('content')
-        @yield('__content')
-    @endsection
-@else
-    <p>
-        @yield('__content')
-    </p>
-    <p>
-        Kindly,<br>
-        {{ env('APP_NAME') }}
-    </p>
-@endif
+<p>
+    @yield('__content')
+</p>
+<p>
+    Kindly,<br>
+    {{ env('APP_NAME') }}
+</p>
