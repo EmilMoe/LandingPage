7e639ac70a902ffdec30fb465809bf05607b0a16
Emil Moe <emil@moegroup.dk>
2020-05-04 10:58:11 +0200

    Go to signup on errors

--- src/resources/views/components/signup.blade.php
+++ src/resources/views/components/signup.blade.php
@@ -1,42 +1,54 @@
-<section class="section text-center" id="signup">
-    <div class="container">
-        <header class="section-header">
-            <h2>{{ $title ?? 'Sign up for free' }}</h2>
-            <hr>
-        </header>
-        <div class="row">
-            <form class="col-md-5 col-xl-6 mx-auto input-border" method="POST" action="{{ route('register') }}">
-                @csrf
-                <div class="form-group">
-                    <input type="text" name="name" class="form-control form-control-lg  @error('name') is-invalid @enderror" value="{{ old('name') }}" required autocomplete="name" placeholder="Name">
-                    @error('name')
-                        <span class="invalid-feedback" role="alert">
-                            <strong>{{ $message }}</strong>
-                        </span>
-                    @enderror
-                    </div>
+@guest
+    @if(session()->has('errors'))
+        <script>
+            document.addEventListener('DOMContentLoaded', function() {
+                document.getElementById('signup').scrollIntoView({
+                    behavior: 'smooth'
+                });
+            }, false);
+        </script>
+    @endif
+    
+    <section class="section text-center" id="signup">
+        <div class="container">
+            <header class="section-header">
+                <h2>{{ $title ?? 'Sign up for free' }}</h2>
+                <hr>
+            </header>
+            <div class="row">
+                <form class="col-md-5 col-xl-6 mx-auto input-border" method="POST" action="{{ route('register') }}">
+                    @csrf
                     <div class="form-group">
-                        <input type="text" name="email" class="form-control form-control-lg  @error('email') is-invalid @enderror" placeholder="Email" value="{{ old('email') }}" required autocomplete="email">
-                        @error('email')
+                        <input type="text" name="name" class="form-control form-control-lg  @error('name') is-invalid @enderror" value="{{ old('name') }}" required autocomplete="name" placeholder="Name">
+                        @error('name')
                             <span class="invalid-feedback" role="alert">
                                 <strong>{{ $message }}</strong>
                             </span>
                         @enderror
+                        </div>
+                        <div class="form-group">
+                            <input type="text" name="email" class="form-control form-control-lg  @error('email') is-invalid @enderror" placeholder="Email" value="{{ old('email') }}" required autocomplete="email">
+                            @error('email')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                            @enderror
+                        </div>
+                        <div class="form-group">
+                            <input type="password" name="password" class="form-control form-control-lg  @error('password') is-invalid @enderror" placeholder="Password" required autocomplete="new-password">
+                            @error('password')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                            @enderror
+                        </div>
+                        <div class="form-group">
+                            <input type="password" name="password_confirmation" class="form-control form-control-lg" placeholder="Confirm password"  required autocomplete="new-password">
+                        </div>
+                        <button class="btn btn-block btn-xl btn-success">Sign up</button>
                     </div>
-                    <div class="form-group">
-                        <input type="password" name="password" class="form-control form-control-lg  @error('password') is-invalid @enderror" placeholder="Password" required autocomplete="new-password">
-                        @error('password')
-                            <span class="invalid-feedback" role="alert">
-                                <strong>{{ $message }}</strong>
-                            </span>
-                        @enderror
-                    </div>
-                    <div class="form-group">
-                        <input type="password" name="password_confirmation" class="form-control form-control-lg" placeholder="Confirm password"  required autocomplete="new-password">
-                    </div>
-                    <button class="btn btn-block btn-xl btn-success">Sign up</button>
-                </div>
-            </form>
+                </form>
+            </div>
         </div>
-    </div>
-</section>
+    </section>
+@endguest
