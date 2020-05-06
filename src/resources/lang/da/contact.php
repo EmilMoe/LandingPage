fba17d2b96a25972e275ea8334a7e843df498a5c
Emil Moe <emil@moegroup.dk>
2020-05-06 12:21:59 +0200

    Danish

---
+++ src/resources/lang/da/contact.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'title' => 'Kontakt',
+    'info' => 'Har du nogle spørgsmål kan du kontakte os og vi vil vende tilbage hurtigst muligt.',
+    'success' => 'Din besked blev modtaget og du vil høre fra os snarest.',
+    'name' => 'Dit Navn',
+    'phone' => 'Dit Telefonnummer',
+    'email' => 'Din Email Addresse',
+    'message' => 'Din Besked',
+    'button' => 'Send Besked',
+    'vat' => 'VAT',
+];
