732a8dc5b4192522c15c54463b99e3ed94491656
Emil Moe <emil@moegroup.dk>
2020-05-06 13:36:18 +0200

    Bugfix

--- src/resources/views/components/process.blade.php
+++ src/resources/views/components/process.blade.php
@@ -8,21 +8,27 @@
 
         @foreach($steps as $step)
             <div class="row gap-y align-items-center py-7">
-                <div class="col-md-6 text-center">
-                    @if(isset($step['image']))
-                        <img src="{{ $step['image'] }}" alt="{{ $step['title'] }}">
-                    @endif
-                </div>
-                
                 @if($loop->odd)
                     <div class="col-md-5 mr-auto text-center text-md-left">
                 @else
+                    <div class="col-md-6 text-center">
+                        @if(isset($step['image']))
+                            <img src="{{ $step['image'] }}" alt="{{ $step['title'] }}">
+                        @endif
+                    </div>
                     <div class="col-md-5 ml-auto text-center text-md-left">
                 @endif
                     <p class="lead-9 fw-900 lh-1 opacity-10">{{ str_pad(($loop->index + 1), 2, '0', STR_PAD_LEFT) }}</p>
                     <h3>{{ $step['title'] }}</h3>
                     <p>{!! $step['info'] !!}</p>
                 </div>
+                @if($loop->odd)
+                    <div class="col-md-6 text-center order-first order-md-last">
+                        @if(isset($step['image']))
+                            <img src="{{ $step['image'] }}" alt="{{ $step['title'] }}">
+                        @endif
+                    </div>
+                @endif
             </div>
         @endforeach
     </div>
